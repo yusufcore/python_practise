1a3fd99721555f89aba64902326c61c73b7463cd
yusuf-in <mohammadyusuf.work@gmail.com>
2026-06-07 16:18:48 +0000

    learnt about different types of variables

--- fundamentals/variables.py
+++ fundamentals/variables.py
@@ -1,7 +1,27 @@
 name = "Yusuf"
 age = 20
 cgpa = 8
+height = 5.7
+is_student = True
 
 print(name)
 print(age)
 print(cgpa)
+print(height)
+print(is_student)
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+print(type(height))
+print(type(is_student))
+
+college = "Integral University, Lucknow"
+branch = "Computer Science and Engineering"
+semester = 4
+cgpa = 8
+
+print("College:", college)
+print("Branch:", branch)
+print("Semester:", semester)
+print("CGPA:", cgpa)
