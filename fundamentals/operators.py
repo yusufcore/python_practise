6f072244f7ffb371947d091404a58bdf7100ac94
yusuf-in <mohammadyusuf.work@gmail.com>
2026-06-08 05:05:25 +0000

    operators in python

---
+++ fundamentals/operators.py
@@ -0,0 +1,11 @@
+a = 14
+b = 27
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+print(a%b)
+print(a//b) # used for floor division, it gives the quotient without the decimal part.
+print(a**b) #  for raised to the power of te number.
