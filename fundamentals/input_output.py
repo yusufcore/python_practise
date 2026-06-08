1daf86da8f6c26d7c282f2003062cf2a9139bb6d
yusuf-in <mohammadyusuf.work@gmail.com>
2026-06-08 05:01:09 +0000

    few changes made

--- fundamentals/input_output.py
+++ fundamentals/input_output.py
@@ -1,11 +1,11 @@
 name = input("Enter your name please: ")
-age = input("Enter your age please: ")
+age = int(input("Enter your age please: "))
 print("Hello", name)
 print("Your age is", age)
 
 print(type(name))
 print(type(age))    # input function always returns a string value, so the type of age will also be string.    
-
+print(age + 15)
 a = 13
 b = 67
 
