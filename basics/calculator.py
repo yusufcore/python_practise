7757e925a7dd307de008124209d92e5faecb1e03
yusuf-in <mohammadyusuf.work@gmail.com>
2026-06-08 05:17:28 +0000

    made a simple calculator in python

---
+++ basics/calculator.py
@@ -0,0 +1,16 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+add = a + b
+subtract = a - b
+multiply = a * b
+divide = a/b
+remainder = a%b
+power = a**b
+
+print(f"The sume of {a} and {b} is: {add}")
+print(f"The difference of {a} and {b} is: {subtract}")
+print(f"The product of {a} and {b} is: {multiply}")
+print(f"The quotient of {a} and {b} is: {divide}")
+print(f"The remainder when {a} is divided by {b} is: {remainder}")
+print(f"{a} raised to the power of {b} is: {power}")
